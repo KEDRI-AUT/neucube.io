e27d8a1dc67710d26294a5d01f68a921a40315da
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-11-21 17:39:52 +1300

    particle settings added

--- themes/neucube-landing/assets/js/custom.js
+++ themes/neucube-landing/assets/js/custom.js
@@ -1,50 +1,110 @@
-if (document.getElementById("search-input") != null) {
-    search.addWidget(
-        instantsearch.widgets.searchBox({
-            container: '#search-input',
-            placeholder: 'Search...',
-            magnifier: false,
-            reset: false
-        })
-    );
-}
-
-if (document.getElementById("hits") != null) {
-    search.addWidget(
-        instantsearch.widgets.hits({
-            container: '#hits',
-            templates: {
-                empty: 'No results',
-                item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#000000"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
             },
-            escapeHits: true
-        })
-    );
-}
-
-if (document.getElementById("search-input-sidenav") != null) {
-    search.addWidget(
-        instantsearch.widgets.searchBox({
-            container: '#search-input-sidenav',
-            placeholder: 'Search...',
-            magnifier: false,
-            reset: false
-        })
-    );
-}
-
-if (document.getElementById("hits-sidenav") != null) {
-    search.addWidget(
-        instantsearch.widgets.hits({
-            container: '#hits-sidenav',
-            templates: {
-                empty: 'No results',
-                item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
+            "polygon": {
+                "nb_sides": 5
             },
-            escapeHits: true
-        })
-    );
-}
-
-
-search.start();
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#981616",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
